7b26211e1afc1d1bcee73e194f779c6199b4c70d
wangyongqi <wangyongqi@baidu.com>
2020-01-14 17:15:48 +0800

    webpack hello world

---
+++ webpack3/webpack.dev.config.js
@@ -0,0 +1,39 @@
+/**
+ * @author: wangyongqi@baidu.com
+ * @date: 2020-01-14 16:54:00
+ * @last Modified time: 2020-01-14 16:54:00
+ */
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:6].js'
+    },
+    module: {
+        rules: [
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            chunks: ['index', 'vendor'],
+            hash: true, // 防止缓存
+            minify: {
+                removeAttributeQuotes: true // 压缩 去掉引号
+            }
+        })
+    ]
+
+};
+
