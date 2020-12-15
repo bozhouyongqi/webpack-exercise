cccab138ac646d1575a34bbac281ad56bbfa11b2
wangyongqi <wangyongqi@baidu.com>
2020-12-15 17:17:16 +0800

    配置webpack基础环境

---
+++ exercise/dist/main.27c5c1.bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+console.log('222');
+/******/ })()
+;
+//# sourceMappingURL=main.27c5c1.bundle.js.map
---
+++ exercise/webpack.config.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: wangyongqi@baidu.com 
+ * @Date: 2020-12-15 17:03:44 
+ * @Last Modified by: wangyongqi@baidu.com
+ * @Last Modified time: 2020-12-15 17:12:22
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 清空打包目录的插件
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:6].bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            hash: true, // 防止缓存
+            minify: {
+                removeAttributeQuotes: true // 压缩 去掉引号
+            }
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        // compress: true,
+        port: 8000,
+        open: true
+    },
+    devtool: 'source-map'
+}
